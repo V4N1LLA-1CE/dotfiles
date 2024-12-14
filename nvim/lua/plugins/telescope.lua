829b01269910cb6ec97fbd127c7a120532177b2d
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-15 01:59:27 +1100

    fix telescope fuzzy

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,25 +2,34 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.6",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    defaults = {
-      path_display = { "truncate" },
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--glob=!**/.git/*",
-        "-g=!**/node_modules/*",
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        path_display = { "truncate" },
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--glob=!**/.git/*",
+          "-g=!**/node_modules/*",
+        },
+        fuzzy = false,
+        search_dirs = { "." },
+        file_ignore_patterns = { "node_modules", ".git" },
+        generic_sorter = require("telescope.sorters").get_substr_matcher,
+        file_sorter = require("telescope.sorters").get_substr_matcher,
       },
-      fuzzy = false, -- Disable fuzzy searching
-      search_dirs = { "." },
-      file_ignore_patterns = { "node_modules", ".git" },
-      generic_sorter = require("telescope.sorters").get_substr_matcher, -- Use substring matcher instead
-      file_sorter = require("telescope.sorters").get_substr_matcher, -- Use substring matcher for files too
-    },
-  },
+    })
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+  end,
 }
