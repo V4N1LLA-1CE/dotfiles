d0c518dddbfc688087a7be8a224ee7f0a9d02f10
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-28 00:00:56 +1100

    modify lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,111 +2,50 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    -- local custom_rosepine = require("lualine.themes.horizon")
+    -- Powerline Separators
+    local powerline = {
+      right = "\u{e0b0}",
+      left = "\u{e0b2}",
+      right_filled = "\u{e0b1}",
+      left_filled = "\u{e0b3}",
+    }
 
-    -- Rose Pine colors
-    -- local colors = {
-    --   base = "#191724",
-    --   surface = "#1f1d2e",
-    --   overlay = "#26233a",
-    --   muted = "#6e6a86",
-    --   subtle = "#908caa",
-    --   text = "#e0def4",
-    --   love = "#eb6f92",
-    --   gold = "#f6c177",
-    --   rose = "#ebbcba",
-    --   pine = "#31748f",
-    --   foam = "#9ccfd8",
-    --   iris = "#c4a7e7",
-    -- }
+    -- Round Separators
+    local round = {
+      right = "\u{e0b4}",
+      left = "\u{e0b6}",
+      right_filled = "\u{e0b5}",
+      left_filled = "\u{e0b7}",
+    }
 
-    -- Setting nils and colors for each mode
-    -- custom_rosepine.normal.c.bg = nil
-    -- custom_rosepine.insert.c.bg = nil
-    -- custom_rosepine.command.c.bg = nil
-    -- custom_rosepine.visual.c.bg = nil
-    -- custom_rosepine.normal.c.fg = colors.muted -- Using muted for regular text
-    -- custom_rosepine.insert.c.fg = colors.muted
-    -- custom_rosepine.command.c.fg = colors.muted
-    -- custom_rosepine.visual.c.fg = colors.muted
-    --
-    -- custom_rosepine.normal.a.bg = colors.iris
-    -- custom_rosepine.insert.a.bg = colors.love
-    -- custom_rosepine.command.a.bg = colors.foam
-    -- custom_rosepine.visual.a.bg = colors.gold
-    --
-    -- custom_rosepine.normal.b.bg = "#191724"
-    -- custom_rosepine.insert.b.bg = "#191724"
-    -- custom_rosepine.visual.b.bg = "#191724"
-    -- custom_rosepine.command.b.bg = "#191724"
-    --
-    -- custom_rosepine.normal.b.fg = colors.iris
-    -- custom_rosepine.insert.b.fg = colors.love
-    -- custom_rosepine.visual.b.fg = colors.gold
-    -- custom_rosepine.command.b.fg = colors.foam
+    -- Additional Separators
+    local extra = {
+      block = "\u{2588}",
+      vertical = "\u{2502}",
+      vertical_thick = "\u{2503}",
+      right_triangle = "\u{e0ba}",
+      left_triangle = "\u{e0bc}",
+      right_semi = "\u{e0bb}",
+      left_semi = "\u{e0bd}",
+    }
 
-    -- local custom_horizon = require("lualine.themes.horizon")
-    -- custom_horizon.normal.c.bg = nil
-    -- custom_horizon.insert.c.bg = nil
-    -- custom_horizon.command.c.bg = nil
-    -- custom_horizon.visual.c.bg = nil
-    --
-    -- custom_horizon.normal.c.fg = "#5e81ac"
-    -- custom_horizon.insert.c.fg = "#5e81ac"
-    -- custom_horizon.command.c.fg = "#5e81ac"
-    -- custom_horizon.visual.c.fg = "#5e81ac"
-    --
-    -- custom_horizon.normal.a.bg = "#b48ead"
-    -- custom_horizon.insert.a.bg = "#ebcb8b"
-    -- custom_horizon.command.a.bg = "#44ffb1"
-    -- custom_horizon.visual.a.bg = "#eceff4"
-    --
-    -- custom_horizon.normal.b.bg = nil
-    -- custom_horizon.insert.b.bg = nil
-    -- custom_horizon.visual.b.bg = nil
-    -- custom_horizon.command.b.bg = nil
-    --
-    -- custom_horizon.normal.b.fg = "#8fbcbb"
-    -- custom_horizon.insert.b.fg = "#8fbcbb"
-    -- custom_horizon.visual.b.fg = "#8fbcbb"
-    -- custom_horizon.command.b.fg = "#8fbcbb"
-
-    -- custom_horizon.normal.a.bg = "#c8c093"
-    -- custom_horizon.insert.a.bg = "#7aa89f"
-    -- custom_horizon.command.a.bg = "#98bb6c"
-    -- custom_horizon.visual.a.bg = "#938aa9"
-    --
-    -- custom_horizon.normal.b.bg = "#090618"
-    -- custom_horizon.insert.b.bg = "#090618"
-    -- custom_horizon.visual.b.bg = "#090618"
-    -- custom_horizon.command.b.bg = "#090618"
-    --
-    -- custom_horizon.normal.b.fg = "#dcd7ba"
-    -- custom_horizon.insert.b.fg = "#dcd7ba"
-    -- custom_horizon.visual.b.fg = "#dcd7ba"
-    -- custom_horizon.command.b.fg = "#dcd7ba"
-
-    -- custom_horizon.normal.a.bg = "#FFE073"
-    -- custom_horizon.insert.a.bg = "#44ffb1"
-    -- custom_horizon.command.a.bg = "#cbe0f0"
-    -- custom_horizon.visual.a.bg = "#a277ff"
-    --
-    -- custom_horizon.normal.b.fg = "#24EAF7"
-    -- custom_horizon.insert.b.fg = "#24EAF7"
-    -- custom_horizon.visual.b.fg = "#24EAF7"
-    -- custom_horizon.command.b.fg = "#24EAF7"
-    --
-    -- custom_horizon.normal.b.bg = "#090618"
-    -- custom_horizon.insert.b.bg = "#090618"
-    -- custom_horizon.visual.b.bg = "#090618"
-    -- custom_horizon.command.b.bg = "#090618"
+    -- custom theme with transparent middle section
+    -- themes:
+    -- horizon, everforest, dracula, modus-vivendi, catppuccin
+    local custom = require("lualine.themes.horizon") -- !!!change lualine theme here!!!
+    custom.normal.c.bg = "none"
+    custom.insert.c.bg = "none"
+    custom.visual.c.bg = "none"
+    custom.replace.c.bg = "none"
+    custom.command.c.bg = "none"
+    custom.inactive.c.bg = "none"
 
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "catppuccin",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        theme = custom,
+        component_separators = { left = round.right_filled, right = round.left_filled },
+        section_separators = { left = round.right, right = round.left },
         disabled_filetypes = {
           statusline = {},
           winbar = {},
@@ -121,20 +60,23 @@ return {
         },
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_a = { { "mode", separator = { left = round.left, right = round.right } } },
+        lualine_b = { "filename", "branch" },
+
+        -- center components
+        lualine_c = {},
+        lualine_x = {},
+
+        lualine_y = { "filetype", "progress" },
+        lualine_z = { { "location", separator = { right = round.right, left = round.left }, left_padding = 2 } },
       },
       inactive_sections = {
-        lualine_a = {},
+        lualine_a = { "filename" },
         lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
+        lualine_c = {},
+        lualine_x = {},
         lualine_y = {},
-        lualine_z = {},
+        lualine_z = { "location" },
       },
       tabline = {},
       winbar = {},
