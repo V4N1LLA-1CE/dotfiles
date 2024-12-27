2fbc18a2e793495b2a2090b0d28e93c418c845f3
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-28 05:38:25 +1100

    nicer lualine configs

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -31,7 +31,7 @@ return {
 
     -- custom theme with transparent middle section
     -- themes:
-    -- horizon, everforest, dracula, modus-vivendi, catppuccin
+    -- horizon, everforest, dracula, modus-vivendi, catppuccin, rose-pine
     local custom = require("lualine.themes.horizon") -- !!!change lualine theme here!!!
     custom.normal.c.bg = "none"
     custom.insert.c.bg = "none"
@@ -40,11 +40,41 @@ return {
     custom.command.c.bg = "none"
     custom.inactive.c.bg = "none"
 
+    -- local custom_nord = {
+    --   normal = {
+    --     a = { fg = "#2E3440", bg = "#88C0D0" },
+    --     b = { fg = "#D8DEE9", bg = "#4C566A" },
+    --     c = { fg = "#D8DEE9", bg = "NONE" },
+    --   },
+    --   inactive = {
+    --     a = { fg = "#D8DEE9", bg = "#3B4252" },
+    --     b = { fg = "#D8DEE9", bg = "#3B4252" },
+    --     c = { fg = "#D8DEE9", bg = "NONE" },
+    --   },
+    -- }
+    --
+    -- -- Then copy the base normal mode for other modes
+    -- custom_nord.insert = vim.deepcopy(custom_nord.normal)
+    -- custom_nord.visual = vim.deepcopy(custom_nord.normal)
+    -- custom_nord.replace = vim.deepcopy(custom_nord.normal)
+    -- custom_nord.command = vim.deepcopy(custom_nord.normal)
+    --
+    -- -- Finally override the colors for each mode's section a
+    -- custom_nord.insert.a = { fg = "#2E3440", bg = "#A3BE8C" }
+    -- custom_nord.visual.a = { fg = "#2E3440", bg = "#B48EAD" }
+    -- custom_nord.replace.a = { fg = "#2E3440", bg = "#BF616A" }
+    -- custom_nord.command.a = { fg = "#2E3440", bg = "#EBCB8B" }
+
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "catppuccin",
+        theme = custom,
         component_separators = { left = round.right_filled, right = round.left_filled },
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 100,
+        },
         section_separators = { left = round.right, right = round.left },
         disabled_filetypes = {
           statusline = {},
@@ -53,22 +83,24 @@ return {
         ignore_focus = {},
         always_divide_middle = true,
         globalstatus = true,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
       },
       sections = {
         lualine_a = { { "mode", separator = { left = round.left, right = round.right } } },
-        lualine_b = { "filename", "branch" },
+        lualine_b = {
+          "filename",
+          {
+            "branch",
+            icon = "\u{f418}",
+            colored = true,
+          },
+        },
 
         -- center components
         lualine_c = {},
         lualine_x = {},
 
         lualine_y = { "filetype", "progress" },
-        lualine_z = { { "location", separator = { right = round.right, left = round.left }, left_padding = 2 } },
+        lualine_z = { { "location", separator = { right = round.right, left = round.left } } },
       },
       inactive_sections = {
         lualine_a = { "filename" },
