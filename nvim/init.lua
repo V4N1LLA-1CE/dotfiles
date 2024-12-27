69f2c4378bd7c4ea50001d2e6deab3335ce1aaea
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-28 06:57:36 +1100

    remove nvim notify and kanagawa compile command

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- vim.api.nvim_create_autocmd("VimEnter", {
---   callback = function()
---     vim.cmd("KanagawaCompile")
---     vim.cmd("set laststatus=0")
---   end,
--- })
