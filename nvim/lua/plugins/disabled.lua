80eda8bbbcc353bbda7699e2289fd4105c782abc
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-08 20:59:36 +1000

    feat: disable indents

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,16 @@
 return {
   { "echasnovski/mini.indentscope", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      -- disable indent scope and lines
+      indent = {
+        enabled = false,
+      },
+      scope = {
+        enabled = false,
+      },
+    },
+  },
 }
