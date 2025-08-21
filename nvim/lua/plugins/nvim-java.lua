e3855d67e319d8319f98387974b5e0ef06d3a5ea
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-22 02:57:50 +1000

    change

--- nvim/lua/plugins/nvim-java.lua
+++ nvim/lua/plugins/nvim-java.lua
@@ -1,46 +1,12 @@
 return {
-  "nvim-java/nvim-java",
-  config = false,
-  enabled = false,
+  "elmcgill/springboot-nvim",
   dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          -- Your JDTLS configuration goes here
-          jdtls = {
-            -- settings = {
-            --   java = {
-            --     configuration = {
-            --       runtimes = {
-            --         {
-            --           name = "JavaSE-23",
-            --           path = "/usr/local/sdkman/candidates/java/23-tem",
-            --         },
-            --       },
-            --     },
-            --   },
-            -- },
-          },
-        },
-        setup = {
-          jdtls = function()
-            -- Your nvim-java configuration goes here
-            require("java").setup({
-              -- root_markers = {
-              --   "settings.gradle",
-              --   "settings.gradle.kts",
-              --   "pom.xml",
-              --   "build.gradle",
-              --   "mvnw",
-              --   "gradlew",
-              --   "build.gradle",
-              --   "build.gradle.kts",
-              -- },
-            })
-          end,
-        },
-      },
-    },
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-jdtls",
+    "nvim-tree/nvim-tree.lua",
   },
+  config = function()
+    local springboot_nvim = require("springboot-nvim")
+    springboot_nvim.setup({})
+  end,
 }
