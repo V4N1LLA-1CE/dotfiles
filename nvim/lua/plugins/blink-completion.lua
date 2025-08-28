2600ed6edd985c0cb33e4f0816d744aca26a53df
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-29 05:01:35 +1000

    BEAUTIFUL AUTOCOMPLETE MENU

--- nvim/lua/plugins/blink-completion.lua
+++ nvim/lua/plugins/blink-completion.lua
@@ -5,5 +5,56 @@ return {
       preset = "default",
       ["<Tab>"] = { "accept", "fallback" },
     },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+    },
+    completion = {
+      menu = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+        scrollbar = true,
+      },
+    },
+    documentation = {
+      window = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+      },
+    },
   },
+  config = function(_, opts)
+    require("blink.cmp").setup(opts)
+    
+    -- Custom highlight groups for proper rounded border background
+    vim.api.nvim_set_hl(0, "BlinkCmpMenu", {
+      bg = "NONE",
+      fg = vim.api.nvim_get_hl(0, { name = "Normal" }).fg,
+    })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", {
+      bg = "NONE",
+      fg = "#FF7AB2", -- Xcode pink for border
+    })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", {
+      bg = "#5C2E91",
+      fg = "#FF7AB2",
+    })
+    
+    -- Scrollbar styling to match theme
+    vim.api.nvim_set_hl(0, "PmenuSbar", {
+      bg = "NONE",
+    })
+    vim.api.nvim_set_hl(0, "PmenuThumb", {
+      bg = "#FF7AB2",
+    })
+    
+    -- Documentation window styling
+    vim.api.nvim_set_hl(0, "BlinkCmpDoc", {
+      bg = "NONE",
+      fg = vim.api.nvim_get_hl(0, { name = "Normal" }).fg,
+    })
+    vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", {
+      bg = "NONE",
+      fg = "#FF7AB2",
+    })
+  end,
 }
