575217cf1fb97628fa2a5d062d331cc27189ee66
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-02-06 22:52:56 +0700

    feat: nicer bufferline and incline settings

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,12 +6,29 @@ return {
     vim.opt.termguicolors = true
 
     require("bufferline").setup({
+      highlights = {
+        fill = { bg = "none" },
+        background = { bg = "none" },
+        buffer_selected = { bg = "none", bold = true, italic = false },
+        buffer_visible = { bg = "none" },
+        separator = { bg = "none" },
+        separator_selected = { bg = "none" },
+        separator_visible = { bg = "none" },
+        indicator_selected = { bg = "none" },
+      },
       options = {
-        separator_style = { "", "" },
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        indicator = {
-          style = "underline",
+        separator_style = "thin", -- "slant", "padded_slant", "thin"
+        indicator = { style = "none" },
+        always_show_bufferline = false,
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+            bg = "none",
+          },
         },
       },
     })
